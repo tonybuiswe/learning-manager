970a6b6262a9824b2e9aea8ace2800ee4c26bc25
Tony Bui <tbui.vdev@gmail.com>
2023-05-07 11:18:53 -0500

    post router

--- back-end/routes/postRoute.js
+++ back-end/routes/postRoute.js
@@ -29,7 +29,9 @@ router.post("/", verifyToken, async (req, res) => {
     res.json({ success: "true", message: "Post successfully", post: newPost });
   } catch (error) {
     console.log(e);
-    return res.status(500).json({ success: false, message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
   }
 });
 
@@ -39,14 +41,57 @@ router.post("/", verifyToken, async (req, res) => {
 router.get("/", verifyToken, async (req, res) => {
   try {
     const posts = await PostModel.find({ user: req.userId }).populate({
-      path: 'user',
-      select: '-password'
+      path: "user",
+      select: "-password",
     });
     res.json({ success: true, posts });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ success: false, message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+});
 
+router.put(":/id", verifyToken, async (req, res) => {
+  const { title, description, url, status, userId } = req.body;
+
+  if (!title)
+    return res
+      .status(400)
+      .json({ success: false, message: "Title is required" });
+
+  try {
+    let updatedPost = {
+      title,
+      description: description || "",
+      status,
+      url: url ? (url.startsWith("https://") ? url : `https:${url}`) : "",
+    };
+
+    const updateCondition = { _id: req.params.id, user: req.userId };
+    //  {new:true} the updatedPost will be the new one instead of the old one
+    updatedPost = await PostModel.findOneAndUpdate(
+      updateCondition,
+      updatedPost,
+      { new: true }
+    );
+
+    //   User bot authorized to update post
+    if (!updatedPost) {
+      return res
+        .status(401)
+        .json({
+          succes: false,
+          message: "Post not found or user not authorized",
+        });
+    }
+    return res.json({success:"true",message:""})
+  } catch (error) {
+    console.log(e);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
   }
 });
 module.exports = router;
